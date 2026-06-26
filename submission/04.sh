ca836b64be4b6f1068bf8f68b710bf32053360d6
Edwin Mwenda Mwiti <eduedwyn5@gmail.com>
2026-06-26 23:15:46 +0300

    Add mining to 04.sh to ensure wallet has balance

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 bitcoin-cli -regtest createwallet "builderswallet"
+# Mine 101 blocks to get spendable coins
+ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 $ADDR
